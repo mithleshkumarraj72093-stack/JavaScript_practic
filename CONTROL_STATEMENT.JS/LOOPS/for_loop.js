03fbedb0a94a3d2cb00c7bd5859bca951e7ed38d
class1-demo <mithileshkumar72093@gmail.com>
2026-07-04 20:53:01 +0530

    question- Answer

--- CONTROL_STATEMENT.JS/LOOPS/for_loop.js
+++ CONTROL_STATEMENT.JS/LOOPS/for_loop.js
@@ -6,6 +6,50 @@ const prompt = require("prompt-sync")();
 //   console.log(i);
 // }
 
-for (let i = 97; i <= 122; i++) {
-  console.log(String.fromCharCode(i));
+// for (let i = 97; i <= 122; i++) {
+//   console.log(String.fromCharCode(i));
+// }
+
+
+// 2.  for loop se 10 se 1 tak reverse order me print karo.
+
+// for (let i = 10; i >= 1; i--){
+//   console.log(i);
+  
+// }
+
+// 3. 1 to 100 even number
+
+// for (let i = 2; i <= 100; i+=2){
+//   console.log(i);
+  
+// }
+//4. 1 to 100 odd number 
+
+
+// for (let i = 1; i <= 100; i+=2){
+//   console.log(i);
+  
+// }
+// 100 to 1 
+
+// for (let i = 99; i >= 1; i-=2){
+//   console.log(i);
+  
+// }
+
+// for (let i = 99; i >= 1; i -= 2) {
+//   console.log(i);
+// }
+
+// 5 ka table print karo.
+
+// for (let i = 1; i <= 10; i++){
+//   console.log(5*i);
+  
+// }
+
+for (let i = 1; i <= 10; i++){
+  console.log(2*i);
+  
 }
