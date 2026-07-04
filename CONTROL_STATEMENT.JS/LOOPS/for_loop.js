5ec0b38f24d146d3655ef558cbdde6609553432d
class1-demo <mithileshkumar72093@gmail.com>
2026-07-04 14:35:57 +0530

    for loop update file

--- CONTROL_STATEMENT.JS/LOOPS/for_loop.js
+++ CONTROL_STATEMENT.JS/LOOPS/for_loop.js
@@ -1,2 +1,11 @@
 const prompt = require("prompt-sync")();
 
+// 1.  for loop se 1 se 10 tak numbers print karo.
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+for (let i = 97; i <= 122; i++) {
+  console.log(String.fromCharCode(i));
+}
