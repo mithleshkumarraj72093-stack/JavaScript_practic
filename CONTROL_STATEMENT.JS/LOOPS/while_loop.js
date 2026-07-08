783ad77e8a739a8302e6b32b8ec5bb7993d12a20
class1-demo <mithileshkumar72093@gmail.com>
2026-07-09 01:28:23 +0530

    update

--- CONTROL_STATEMENT.JS/LOOPS/while_loop.js
+++ CONTROL_STATEMENT.JS/LOOPS/while_loop.js
@@ -1,1 +1,89 @@
-// Is file me main while loop ka qs practic kar raha hun 
+// 1. ATM Balance Check ?
+
+// const prompt = require("prompt-sync")();
+
+// let totalBal = 5000;
+// let withdrawalAmount = 500;
+// let withdrawCount = 0;
+
+// while(totalBal >=  withdrawalAmount){
+//     totalBal -= withdrawalAmount;
+//     withdrawCount++;
+
+//     console.log(`withdrawal ${withdrawCount}: ${withdrawalAmount}`);
+//     console.log(`withdrawn amount = ${withdrawalAmount}` );
+    
+    
+// }
+// console.log(`Remaining balance = ${totalBal}`);
+// console.log(`total numbers of time withdrawn balance = ${withdrawCount}`)
+
+
+
+
+// In this file i am practicing while loop qs and ans.
+
+// function hello(){
+//     console.log("Mithielsh kumar")
+// }
+// hello();
+
+// function detail(name, age, marks){
+//     console.log("details -> " + "name= "+ name,"age="+ age,"marks= "+ marks)
+// }
+// detail("mithilesh",+ 20, + 98);
+
+// default parameters. 
+
+
+
+// function sum(a,b = 10){
+//     console.log("sum =",a+b);
+    
+// }
+// sum(a=20);
+
+
+// return function
+
+// function sum(a,b){
+//     return a+b
+// }
+// let result = sum(100,20);
+// console.log("sum is = ",result)
+
+// let i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 10;
+// while(i >= 1){
+//     console.log(i);
+//     i--;
+// }
+
+
+// let i = 2;
+// while(i <= 20 ){
+//     console.log(i)
+//     i+=2;
+
+// }
+
+// let i = 1;
+// while(i <= 20 ){
+//     console.log(i)
+//     i+=2;
+
+// }
+
+
+let i = 1;
+
+while(i <= 10){
+    console.log(5*i);
+    i++;
+    
+}
